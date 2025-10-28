6cf4a9a336eb26a9b9b4fe0ff574e9951e3575e8
hareemzia58 <hareemzia58@gmail.com>
2025-10-29 02:56:26 +0500

    Optimized kernel launch

--- src/V3/convolveGPU.cu
+++ src/V3/convolveGPU.cu
@@ -1,3 +1,4 @@
+%%writefile convolveGPU.cu
 /*********************************************************************
  * convolveGPU.cu
  *********************************************************************/
@@ -245,12 +246,12 @@ void convolveImageHorizGPU(
   cudaMemcpy(d_in, imgin->data, imgSize, cudaMemcpyHostToDevice);
   cudaMemcpy(d_kernel, kernel.data, kSize, cudaMemcpyHostToDevice);
 
-  dim3 block(16, 16);
+  dim3 block(32, 8);
   dim3 grid((ncols + block.x - 1) / block.x,
             (nrows + block.y - 1) / block.y);
 
   int R = kernel.width / 2;
-int sharedBytes = (16 + 2*R) * (16) * sizeof(float);  // TILE_W * TILE_H
+int sharedBytes = (32 + 2*R) * (8) * sizeof(float);  // TILE_W * TILE_H
 
 convolveHorizShared<<<grid, block, sharedBytes>>>(
     d_in, d_out,
@@ -285,12 +286,12 @@ void convolveImageVertGPU(
   cudaMemcpy(d_in, imgin->data, imgSize, cudaMemcpyHostToDevice);
   cudaMemcpy(d_kernel, kernel.data, kSize, cudaMemcpyHostToDevice);
 
-  dim3 block(16, 16);
+  dim3 block(32, 8);
   dim3 grid((ncols + block.x - 1) / block.x,
             (nrows + block.y - 1) / block.y);
 
  int R = kernel.width / 2;
-int sharedBytes = (16) * (16 + 2*R) * sizeof(float);  // TILE_W * TILE_H
+int sharedBytes = (32) * (8 + 2*R) * sizeof(float);  // TILE_W * TILE_H
 
 convolveVertShared<<<grid, block, sharedBytes>>>(
     d_in, d_out,
